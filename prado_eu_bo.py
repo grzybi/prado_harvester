1d41f0d60027596260e630a85a1612370cb8c2f9
grzybi <grzybi4spam@gmail.com>
2022-03-17 15:24:29 +0100

    Added cmd parser. Can filter by EU, countries and force downloading. Code not cleaned and not refactored.

--- prado_eu_bo.py
+++ prado_eu_bo.py
@@ -3,34 +3,37 @@ import requests as req
 from document import Document
 # import itertools
 import os
+import argparse
 
 PRADO = str("https://www.consilium.europa.eu/prado")
 
 
 class PradoEuBo:
-    def __init__(self):
+    def __init__(self, force, eu_only, countries, doc_types):
         response = req.get(PRADO + "/en/search-by-document-country.html")
         soup = BeautifulSoup(response.text, "lxml")
         countries_block = soup.find_all(attrs={"class": "council-link-list"})
-        print(type(countries_block))
         country_list = dict()
 
-        print(len(countries_block))
-        for block in countries_block:
-            countries = block.find_all("a")
-            for country in countries:
-                country_name = country.text.split("-")[1]
-                country_name = country_name.split(" * ")[1].split(" *")[0] if "*" in country_name \
-                    else country_name.split(" ")[1]
-                country_list[str(country.text.split(" -")[0])] = country_name
+        if countries:
+            country_list = countries[0]
+            for c in country_list:
+                print(c)
+        else:
+            print("->" + str(len(countries_block)))
 
-        print(country_list)
-        # print(len(country_list))
+            for block in countries_block[:2] if eu_only else countries_block:
+                # print("-->" + str(block))
+                countries = block.find_all("a")
+                for country in countries:
+                    country_name = country.text.split("-")[1]
+                    country_name = country_name.split(" * ")[1].split(" *")[0] if "*" in country_name \
+                        else country_name.split(" ")[1]
+                    country_list[str(country.text.split(" -")[0])] = country_name
 
         documents = list()
 
-        # TODO: check slicing
-        # for c in ["PRK"]:
+        # TODO: add selecting type of document(s)
         for c in country_list:
             print(c)
             if c == "PSE":
@@ -76,18 +79,39 @@ class PradoEuBo:
         os.makedirs("img", exist_ok=True)
 
         for i, d in enumerate(documents):
-            r = req.get(d.image_front)
-            with open(os.path.join("img", d.id + "-front.jpg"), "wb") as outfile:
-                outfile.write(r.content)
-                outfile.close()
+            file_name = os.path.join("img", d.id + "-front.jpg")
+            if not os.path.exists(file_name) or force:
+                r = req.get(d.image_front)
+                with open(os.path.join(file_name), "wb") as outfile:
+                    outfile.write(r.content)
+                    outfile.close()
+            else:
+                print("file_exists: " + file_name)
 
-            r = req.get(d.image_back)
-            with open(os.path.join("img", d.id + "-back.jpg"), "wb") as outfile:
-                outfile.write(r.content)
-                outfile.close()
+            file_name = os.path.join("img", d.id + "-back.jpg")
+            if not os.path.exists(file_name) or force:
+                r = req.get(d.image_back)
+                with open(os.path.join(file_name), "wb") as outfile:
+                    outfile.write(r.content)
+                    outfile.close()
 
             print(f"{i + 1}/{len(documents)}")
 
 
 if __name__ == "__main__":
-    prado = PradoEuBo()
+    # TODO: excluding parameters (i.e. can't use --eu and --country at the same time)
+    # https://stackoverflow.com/questions/49679587/how-to-mutually-exclude-multiple-arguments-in-pythons-argparse
+
+    # TODO: add support for lists
+
+    parser = argparse.ArgumentParser(description="Webscrapper for images from PRADO database.")
+    parser.add_argument('--force', help="Force downloading images", action="store_true")
+    parser.add_argument('--eu', help="Scrap countries only from EU", action="store_true")
+    parser.add_argument('--country', help="Specify countries to scrap", action="append", nargs="+", type=str)
+    parser.add_argument('--doc-type', help="Specify document type(s) to scrap", action="append")
+    # subparsers = parser.add_subparsers(help="sub")
+    # parser_force = subparsers.add_parser("a", help="b")
+    args = parser.parse_args()
+    print(args)
+
+    prado = PradoEuBo(args.force, args.eu, args.country, args.doc_type)
